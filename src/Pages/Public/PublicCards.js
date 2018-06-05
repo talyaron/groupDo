234802327f665223bdaa702aca963d9b131fdd6d
Tal Yaron <tal.yaron@gmail.com>
2018-06-05 08:18:07 +0300

    added keys

--- src/Pages/Public/PublicCards.js
+++ src/Pages/Public/PublicCards.js
@@ -3,10 +3,10 @@ export const PublicCards = {
     view: function (vnode) {
         var cards2 = vnode.attrs.cards;
         return (
-            cards2.map(function (card) {
+            cards2.map(function (card, index) {
                 return (
 
-                    <div class="col s12 m6 l4">
+                    <div class="col s12 m6 l4" key={index}>
                         <div class="card blue-grey darken-1">
                             <div class="card-content white-text">
                                 <span class="card-title">{card.name}</span>
