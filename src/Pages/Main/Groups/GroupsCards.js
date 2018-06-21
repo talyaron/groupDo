88238dba8921031592b55b75fea2eb50efbbe83f
Tal Yaron <tal.yaron@gmail.com>
2018-06-21 16:00:18 +0300

    style cards

--- src/Pages/Main/Groups/GroupsCards.js
+++ src/Pages/Main/Groups/GroupsCards.js
@@ -1,3 +1,4 @@
+import './GroupsCards.css';
 import store from '../../../data/store';
 
 export const GroupCards = {
@@ -8,19 +9,19 @@ export const GroupCards = {
             cards2.map(function (card, index) {
 
                 return (
-                    <div class="col s12 m6 l4" key={index}>
+                    <div class="col s12 m6 l4 right" key={index}>
                         <div
-                            class="card blue-grey darken-1 groupCard"
+                            class="groupCard"
                             onclick={() => {
                                 store.current.group = card;
                                 m.route.set('/group/' + card.id);
 
                             }}
                         >
-                            <div class="card-content white-text">
-                                <span class="card-title">{card.name}</span>
-                                <p>{card.description}</p>
-                            </div>
+
+                            <span class="activityCardTitle">{card.name}</span>
+                            <p>{card.description}</p>
+
                         </div>
                     </div>
 
--- src/Pages/Main/Starts/StartsCards.js
+++ src/Pages/Main/Starts/StartsCards.js
@@ -1,4 +1,4 @@
-
+import './StartsCard.css';
 import store from '../../../data/store';
 
 export const StartsCards = {
@@ -10,18 +10,16 @@ export const StartsCards = {
             cards2.map(function (card, index) {
                 return (
 
-                    <div class="col s12 m6 l4" key={index}
+                    <div class="col s12 m6 l4 right" key={index}
                         onclick={() => {
                             m.route.set('/activity/' + card.id);
                             store.current.action = card;
                         }}
 
                     >
-                        <div class="card white">
-                            <div class="card-content white-black">
-                                <span class="card-title">{card.name}</span>
-                                <p>{card.description}</p>
-                            </div>
+                        <div class="activityCard">
+                            <p class="activityCardTitle">{card.name}</p>
+                            <p>{card.description}</p>
                         </div>
                     </div>
 
